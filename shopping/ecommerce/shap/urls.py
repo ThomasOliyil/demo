80f6451891f8083db401d869b2e9622295f556cf
thomachayan2255 <thomaskuttyoliyil@gmail.com>
2022-10-12 12:53:18 +0530

    remove folder

--- shopping/ecommerce/shap/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name='shap'
-
-urlpatterns = [
-    path('',views.allproductcategory,name='allproductcategory'),
-    path('<slug:c_slug>/',views.allproductcategory,name='product_by_category'),
-    path('<slug:c_slug>/<slug:product_slug>/',views.prodetails,name='productcategory'),
-]
